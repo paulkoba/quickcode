10432be36cbb8320720362f1054351103f637f68
Paul Koba <paul.koba@pm.me>
2021-10-30 21:46:55 +0000

    Base for implementing testing

--- quickcode/website/models.py
+++ quickcode/website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.utils.translation import gettext_lazy as _
 
 
 class Problem(models.Model):
@@ -20,3 +21,27 @@ class TestCase(models.Model):
     input = models.TextField()
     output = models.TextField()
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    executionLimit = models.IntegerField()
+
+
+class Submission(models.Model):
+    """Model representing a submission for a particular problem."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular submission')
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+
+    class Result(models.TextChoices):
+        SUCCESS = 'S', _('Successful')
+        TIMEOUT = 'T', _('Time Limit Exceeded')
+        WRONG = 'W', _('Wrong Answer')
+        UNKNOWN = 'U', _('Unknown')
+
+    result = models.CharField(max_length=1,
+                              choices=Result.choices,
+                              default=Result.UNKNOWN, )
+
+    submissionTime = models.DateTimeField(auto_now=True)
+
+
+class SubmissionResultTestCase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    submissionID = models.ForeignKey(Submission, on_delete=models.CASCADE)
--- quickcode/website/views.py
+++ quickcode/website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Problem
+from .models import Problem, TestCase, Submission
 
 
 def index(request):
@@ -14,18 +14,15 @@ def index(request):
 
 
 def problem(request, problem_id):
-
     try:
-
         context = {
-            'problem': Problem.objects.get(id=problem_id)
+            'problem': Problem.objects.get(id=problem_id),
+            'submissions': Submission.objects.filter(problem_id=problem_id),
         }
-
         return render(request, 'problem.html', context=context)
 
-    except Problem.DoesNotExist:
+    except (Submission.DoesNotExist, Problem.DoesNotExist):
         return render(request, 'not_found.html')
-
 
 
 def not_found(request, exception):
