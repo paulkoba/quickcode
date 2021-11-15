081f3d1c8319d5d9e63a4390d89f62449d378f0d
Paul Koba <paul.koba@pm.me>
2021-11-15 19:32:24 +0000

    Linted .sh files

--- quickcode/ugc/runner.sh
+++ quickcode/ugc/runner.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 g++ -O3 -march=native ./submission.cpp
 
-index=0
-
 mkdir output/
 
 for filename in tests/*; do
     [ -e "$filename" ] || continue
     name=${filename##*/}
 
-    cat $filename | ./a.out > ./output/$name
-    let index=$index+1
+    < "$filename" ./a.out > ./output/"$name"
 done
